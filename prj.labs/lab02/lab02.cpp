2e3f2a60fa2cc958e0c81de7cc01931cc7cf67ac
MVVladimir <mvv1_1@mail.ru>
2022-02-21 14:05:36 +0300

    lab2 is complited

--- prj.labs/lab02/lab02.cpp
+++ prj.labs/lab02/lab02.cpp
@@ -1,6 +1,16 @@
 #include <opencv2/opencv.hpp>
 #include <vector>
 
+int max(const std::vector<int>& v) {
+
+    int max = v[0];
+
+    for (size_t i = 0; i < v.size(); i += 1)
+        max = max < v[i] ? v[i] : max;
+
+    return max;
+}
+
 void calcHist(std::vector<std::vector<int>>& hist, const cv::Mat& img) {
     for (std::ptrdiff_t i = 0; i < hist.size(); i += 1)
     {
@@ -20,6 +30,17 @@ void calcHist(std::vector<std::vector<int>>& hist, const cv::Mat& img) {
         }
     }
 
+    int max_blue_color = max(hist[0]);
+    int max_green_color = max(hist[1]);
+    int max_red_color = max(hist[2]);
+
+    for (std::ptrdiff_t i = 0; i < hist[0].size(); i += 1)
+    {
+        hist[0][i] = 255 - std::ceil(((double)hist[0][i] / (double)max_blue_color) * 255.);
+        hist[1][i] = 255 - std::ceil(((double)hist[0][i] / (double)max_green_color) * 255.);
+        hist[2][i] = 255 - std::ceil(((double)hist[0][i] / (double)max_red_color) * 255.);
+    }
+
     for (std::ptrdiff_t i = 0; i < hist.size(); i += 1)
     {
         for (std::ptrdiff_t j = 0; j < hist[i].size(); j += 1)
@@ -27,6 +48,13 @@ void calcHist(std::vector<std::vector<int>>& hist, const cv::Mat& img) {
             std::cout << hist[i][j] << " ";
         }
         std::cout << '\n';
+    }
+}
+
+void fillVizHistCol(cv::Mat& vizHist, std::ptrdiff_t const col, std::ptrdiff_t const level) {
+    for (std::ptrdiff_t i = level; i < vizHist.rows; i += 1)
+    {
+        vizHist.at<cv::uint8_t>(i, col) = 255;
     }
 }
 
@@ -39,7 +67,7 @@ int main() {
 
     imwrite("./cross_0256x0256_0256.jpg", img_png, compression_params); // Save as jpg with 75 persents loss
 
-    //=============================================================================================
+    //==========================CALCULATING=AND=MERGING=CHANNELS===============================
 
     std::vector<cv::Mat> channels(3);
     cv::split(img_png, channels); // split img to 3 channels - BGR
@@ -63,11 +91,11 @@ int main() {
     cv::hconcat(upper, upperPic);
     cv::hconcat(bottom, bottomPic);
     cv::vconcat(upperPic, bottomPic, img_channels);
-    
-    imwrite("./cross_0256x0256_png_channels.png", img_channels);
-    imwrite("./cross_0256x0256_jpg_channels.jpg", img_channels, compression_params);
 
-    //=============================================================================================
+    imwrite("./cross_0256x0256_png_channels.png", img_channels);
+    imwrite("./cross_0256x0256_jpg_channels.png", img_channels);
+
+    //======================CALCULATING=HISTOGRAMS=================================================
 
     std::string filename_jpg = "./cross_0256x0256_0256.jpg";
     cv::Mat img_jpg = cv::imread(filename_jpg, cv::IMREAD_COLOR);
@@ -77,5 +105,48 @@ int main() {
 
     calcHist(hist_jpg, img_jpg);
     calcHist(hist_png, img_png);
-     
+
+    // Summary histogram
+    cv::Mat vizHist(256, 256, CV_8UC3, cv::Vec3b(0, 0, 0));
+
+    // Histogram for jpg image - vizHist_jpg - and for every channel BGR
+    cv::Mat vizHist_jpg(256, 256, CV_8UC3, cv::Vec3b(0, 0, 0));
+    std::vector<cv::Mat> vizHist_jpg_levels = { cv::Mat(256, 256, CV_8UC1) ,
+        cv::Mat(256, 256, CV_8UC1) , cv::Mat(256, 256, CV_8UC1) };
+
+    vizHist_jpg_levels[0] = 0;
+    vizHist_jpg_levels[1] = 0;
+    vizHist_jpg_levels[2] = 0;
+
+    // Histogram for png image - vizHist_png - and for every channel BGR
+    cv::Mat vizHist_png(256, 256, CV_8UC3, cv::Vec3b(255, 255, 255));
+    std::vector<cv::Mat> vizHist_png_levels = { cv::Mat(256, 256, CV_8UC1) ,
+        cv::Mat(256, 256, CV_8UC1) , cv::Mat(256, 256, CV_8UC1) };
+
+    vizHist_png_levels[0] = 0;
+    vizHist_png_levels[1] = 0;
+    vizHist_png_levels[2] = 0;
+
+    // Colors templates in BGR
+    cv::Vec3b blue_color = { 255, 0, 0 };
+    cv::Vec3b green_color = { 0, 255, 0 };
+    cv::Vec3b red_color = { 0, 0, 255 };
+
+    for (std::ptrdiff_t i = 0; i < vizHist_jpg.cols; i += 1)
+    {
+        fillVizHistCol(vizHist_jpg_levels[0], i, hist_jpg[0][i]);
+        fillVizHistCol(vizHist_jpg_levels[1], i, hist_jpg[1][i]);
+        fillVizHistCol(vizHist_jpg_levels[2], i, hist_jpg[2][i]);
+
+        fillVizHistCol(vizHist_png_levels[0], i, hist_png[0][i]);
+        fillVizHistCol(vizHist_png_levels[1], i, hist_png[1][i]);
+        fillVizHistCol(vizHist_png_levels[2], i, hist_png[2][i]);
+    }
+
+    cv::merge(vizHist_jpg_levels, vizHist_jpg);
+    cv::merge(vizHist_png_levels, vizHist_png);
+
+    cv::hconcat(std::vector<cv::Mat> { vizHist_jpg, vizHist_png }, vizHist);
+
+    imwrite("./cross_0256x0256_hists.png", vizHist);
 }
